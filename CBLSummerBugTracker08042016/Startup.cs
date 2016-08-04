76732c332c01f20640f6dab0b658a7617d0fa46b
CoderB <noodoggy@gmail.com>
2016-08-04 14:14:00 -0400

    Add project files.

---
+++ CBLSummerBugTracker08042016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CBLSummerBugTracker08042016.Startup))]
+namespace CBLSummerBugTracker08042016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
